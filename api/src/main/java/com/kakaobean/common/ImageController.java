efc50c851ccf0fe1b84ffb9e97802f5b4285ed8f
happysubin <asb0711@gmail.com>
2023-07-10 12:53:44 +0900

    feat: 단일 사진 업로드 api 구현

---
+++ api/src/main/java/com/kakaobean/common/ImageController.java
@@ -0,0 +1,26 @@
+package com.kakaobean.common;
+
+import com.kakaobean.independentlysystem.image.ImageService;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@RestController
+public class ImageController {
+
+    private final ImageService imageService;
+
+    @PostMapping("/images")
+    public ResponseEntity<ImageResponseDto> uploadImage(@RequestParam MultipartFile image) throws IOException {
+        String imageUrl = imageService.upload(image.getInputStream(), image.getOriginalFilename(), image.getSize(), image.getContentType());
+        return ResponseEntity.ok(new ImageResponseDto(imageUrl));
+    }
+}
