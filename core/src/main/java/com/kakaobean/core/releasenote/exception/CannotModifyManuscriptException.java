53b1e9be1959d6c6f1ee7768adcee51cc0d79593
happysubin <asb0711@gmail.com>
2023-07-20 12:33:00 +0900

    fix: 예외 코드 수정

--- core/src/main/java/com/kakaobean/core/releasenote/exception/CannotModifyManuscriptException.java
+++ core/src/main/java/com/kakaobean/core/releasenote/exception/CannotModifyManuscriptException.java
@@ -3,7 +3,7 @@ package com.kakaobean.core.releasenote.exception;
 public class CannotModifyManuscriptException extends ReleaseNoteException {
 
     private static final String message = "릴리즈 노트 원고를 수정할 수 없습니다.";
-    private static final String errorCode = "M005";
+    private static final String errorCode = "Ma005";
     private static final Integer status = 400;
 
     public CannotModifyManuscriptException() {
