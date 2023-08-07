cedfc701b2d3d18fe14a1ec8b349eed40340a3a4
happysubin <asb0711@gmail.com>
2023-08-07 13:57:02 +0900

    feat: 릴리즈 노트 원고 단건 조회 필요한 필드 추가

--- core/src/main/java/com/kakaobean/core/releasenote/domain/repository/query/FindManuscriptResponseDto.java
+++ core/src/main/java/com/kakaobean/core/releasenote/domain/repository/query/FindManuscriptResponseDto.java
@@ -1,5 +1,7 @@
 package com.kakaobean.core.releasenote.domain.repository.query;
 
+import com.kakaobean.core.releasenote.domain.Manuscript;
+import com.kakaobean.core.releasenote.domain.ManuscriptStatus;
 import lombok.Getter;
 
 @Getter
@@ -10,16 +12,22 @@ public class FindManuscriptResponseDto {
     private final String manuscriptTitle;
     private final String manuscriptContent;
     private final String manuscriptVersion;
+    private final String createdAt;
+    private final ManuscriptStatus manuscriptStatus;
 
     public FindManuscriptResponseDto(String lastEditedMemberName,
                                      Long manuscriptId,
                                      String manuscriptTitle,
                                      String manuscriptContent,
-                                     String manuscriptVersion) {
+                                     String manuscriptVersion,
+                                     String createdAt,
+                                     ManuscriptStatus manuscriptStatus) {
         this.lastEditedMemberName = lastEditedMemberName;
         this.manuscriptId = manuscriptId;
         this.manuscriptTitle = manuscriptTitle;
         this.manuscriptContent = manuscriptContent;
         this.manuscriptVersion = manuscriptVersion;
+        this.createdAt = createdAt;
+        this.manuscriptStatus = manuscriptStatus;
     }
 }
