0589f05b1c22348304885625e7a318bf4cae054d
김희찬 <92291084+BetterTomorrowThanToday@users.noreply.github.com>
2023-08-11 11:11:02 +0900

    feat: issue 삭제 API 구현

--- core/src/main/java/com/kakaobean/core/issue/application/IssueService.java
+++ core/src/main/java/com/kakaobean/core/issue/application/IssueService.java
@@ -2,9 +2,14 @@ package com.kakaobean.core.issue.application;
 
 import com.kakaobean.core.issue.application.dto.request.RegisterIssueRequestDto;
 import com.kakaobean.core.issue.domain.Issue;
+import com.kakaobean.core.issue.domain.repository.CommentRepository;
 import com.kakaobean.core.issue.domain.repository.IssueRepository;
+import com.kakaobean.core.issue.exception.IssueAccessException;
+import com.kakaobean.core.issue.exception.NotExistsIssueException;
+import com.kakaobean.core.project.domain.ProjectMember;
 import com.kakaobean.core.project.domain.repository.ProjectMemberRepository;
 import com.kakaobean.core.project.exception.NotExistsProjectMemberException;
+import com.kakaobean.core.sprint.Exception.SprintAccessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +20,8 @@ public class IssueService {
 
     private final IssueRepository issueRepository;
 
+    private final CommentRepository commentRepository;
+
     private final ProjectMemberRepository projectMemberRepository;
 
     @Transactional(readOnly = false)
@@ -24,5 +31,14 @@ public class IssueService {
                 orElseThrow(NotExistsProjectMemberException::new);
         issueRepository.save(issue);
         //알림 필요 없으면 그냥 위 두 줄 합쳐도 됨.
+    }
+
+    @Transactional
+    public void removeIssue(Long memberId, Long issueId){
+        Issue issue = issueRepository.findById(issueId).orElseThrow(NotExistsIssueException::new);
+        ProjectMember projectMember = projectMemberRepository.findByMemberIdAndProjectId(memberId, issue.getProjectId()).orElseThrow(NotExistsProjectMemberException::new);
+        if(projectMember.getMemberId() != issue.getWriterId()){ throw new IssueAccessException(); }
+        issueRepository.delete(issue);
+        commentRepository.deleteByIssueId(issueId);
     }
 }
---
+++ core/src/main/java/com/kakaobean/core/issue/exception/IssueAccessException.java
@@ -0,0 +1,14 @@
+package com.kakaobean.core.issue.exception;
+
+import com.kakaobean.core.sprint.Exception.SprintException;
+
+public class IssueAccessException extends IssueException {
+
+    private static final String message = "이슈 생성 및 변경에 대한 권한이 없습니다.";
+    private static final String errorCode = "I002";
+    private static final Integer status = 400;
+
+    public IssueAccessException() {
+        super(message, status, errorCode);
+    }
+}
