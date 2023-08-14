d99e4b78f40837beaefd25d742062235f349fa29
김희찬 <92291084+BetterTomorrowThanToday@users.noreply.github.com>
2023-08-15 05:38:18 +0900

    feat: 댓글 삭제 API + Test완료

--- core/src/test/java/com/kakaobean/core/integration/issue/CommentServiceTest.java
+++ core/src/test/java/com/kakaobean/core/integration/issue/CommentServiceTest.java
@@ -1,14 +1,17 @@
 package com.kakaobean.core.integration.issue;
 
+import com.kakaobean.core.factory.issue.CommentFactory;
 import com.kakaobean.core.factory.issue.dto.RegisterCommentRequestDtoFactory;
 import com.kakaobean.core.factory.project.ProjectFactory;
 import com.kakaobean.core.factory.sprint.dto.RegisterTaskRequestDtoFactory;
 import com.kakaobean.core.integration.IntegrationTest;
 import com.kakaobean.core.issue.application.CommentService;
 import com.kakaobean.core.issue.application.IssueService;
+import com.kakaobean.core.issue.domain.Comment;
 import com.kakaobean.core.issue.domain.Issue;
 import com.kakaobean.core.issue.domain.repository.CommentRepository;
 import com.kakaobean.core.issue.domain.repository.IssueRepository;
+import com.kakaobean.core.issue.exception.CommentAccessException;
 import com.kakaobean.core.project.domain.Project;
 import com.kakaobean.core.project.domain.ProjectMember;
 import com.kakaobean.core.project.domain.repository.ProjectMemberRepository;
@@ -17,6 +20,7 @@ import com.kakaobean.core.sprint.application.TaskService;
 import com.kakaobean.core.sprint.domain.Sprint;
 import com.kakaobean.core.sprint.domain.repository.SprintRepository;
 import com.kakaobean.core.sprint.domain.repository.TaskRepository;
+import org.assertj.core.api.AbstractThrowableAssert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,7 +29,9 @@ import static com.kakaobean.core.factory.issue.IssueFactory.createIssue;
 import static com.kakaobean.core.factory.project.ProjectMemberFactory.createWithMemberIdAndProjectId;
 import static com.kakaobean.core.factory.sprint.SprintFactory.createWithId;
 import static com.kakaobean.core.project.domain.ProjectRole.ADMIN;
+import static com.kakaobean.core.project.domain.ProjectRole.MEMBER;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CommentServiceTest extends IntegrationTest {
     @Autowired
@@ -63,5 +69,41 @@ public class CommentServiceTest extends IntegrationTest {
 
         // then
         assertThat(commentRepository.findAll().size()).isEqualTo(1);
+    }
+
+    @Test
+    void 댓글을_삭제한다(){
+        //given
+        Project project = projectRepository.save(ProjectFactory.create());
+        ProjectMember projectMember = projectMemberRepository.save(createWithMemberIdAndProjectId(1L, project.getId(), MEMBER));
+
+        Issue issue = issueRepository.save(createIssue(project.getId()));
+
+        Comment comment = commentRepository.save(CommentFactory.createCommentWithMemberIdAndIssueId(projectMember.getMemberId(), issue.getId()));
+
+        //when
+        commentService.removeComment(projectMember.getMemberId(), comment.getId());
+
+        //then
+        assertThat(commentRepository.findAll().size()).isEqualTo(0);
+    }
+
+    @Test
+    void 댓글_작성자만이_댓글을_삭제할_수_있다(){
+        //given
+        Project project = projectRepository.save(ProjectFactory.create());
+        ProjectMember projectMember = projectMemberRepository.save(createWithMemberIdAndProjectId(1L, project.getId(), MEMBER));
+
+        Issue issue = issueRepository.save(createIssue(project.getId()));
+
+        Comment comment = commentRepository.save(CommentFactory.createCommentWithMemberIdAndIssueId(projectMember.getMemberId(), issue.getId()));
+
+        //when
+        AbstractThrowableAssert<?, ? extends Throwable> result = assertThatThrownBy(()->{
+            commentService.removeComment(3L, comment.getId());
+        });
+
+        //then
+        result.isInstanceOf(CommentAccessException.class);
     }
 }
