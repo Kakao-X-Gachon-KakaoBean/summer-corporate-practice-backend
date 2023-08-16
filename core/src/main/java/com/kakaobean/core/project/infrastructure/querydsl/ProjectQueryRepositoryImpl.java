e86f3cad8463e99a94a54ffbc6634c7463b4f337
happysubin <asb0711@gmail.com>
2023-08-16 23:26:19 +0900

    fix: 쿼리 버그 수정

--- core/src/main/java/com/kakaobean/core/project/infrastructure/querydsl/ProjectQueryRepositoryImpl.java
+++ core/src/main/java/com/kakaobean/core/project/infrastructure/querydsl/ProjectQueryRepositoryImpl.java
@@ -55,7 +55,7 @@ public class ProjectQueryRepositoryImpl implements ProjectQueryRepository {
                                 project.content
                         ))
                 .from(project)
-                .join(projectMember).on(projectMember.id.eq(project.id))
+                .join(projectMember).on(projectMember.memberId.eq(member.id))
                 .where(
                         project.status.eq(ACTIVE),
                         projectMember.memberId.eq(memberId)
