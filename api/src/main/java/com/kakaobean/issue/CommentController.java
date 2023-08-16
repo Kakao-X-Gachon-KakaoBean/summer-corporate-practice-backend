d40df1d808377bd2043431a0908dae5973cde645
happysubin <asb0711@gmail.com>
2023-08-16 09:53:58 +0900

    feat: metrics 코드 및 Log 코드 작성

--- api/src/main/java/com/kakaobean/issue/CommentController.java
+++ api/src/main/java/com/kakaobean/issue/CommentController.java
@@ -6,6 +6,7 @@ import com.kakaobean.issue.dto.ModifyCommentRequest;
 import com.kakaobean.issue.dto.ModifyIssueRequest;
 import com.kakaobean.issue.dto.RegisterCommentRequest;
 import com.kakaobean.security.UserPrincipal;
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
+@Timed("api.issue.comment")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -26,14 +28,18 @@ public class CommentController {
     @PostMapping("/comments")
     public ResponseEntity registerComment(@Validated @RequestBody RegisterCommentRequest request,
                                          @AuthenticationPrincipal UserPrincipal userPrincipal){
+        log.info("이슈 댓글 생성 요청 api 시작");
         commentService.registerComment(request.toServiceDto(userPrincipal.getId()));
+        log.info("이슈 댓글 생성 요청 api 종료");
         return new ResponseEntity(CommandSuccessResponse.from("댓글이 생성되었습니다."), CREATED);
     }
 
     @DeleteMapping("/comments/{commentId}")
     public ResponseEntity removeComment(@AuthenticationPrincipal UserPrincipal userPrincipal,
                                       @PathVariable Long commentId) {
+        log.info("이슈 댓글 삭제 요청 api 시작");
         commentService.removeComment(userPrincipal.getId(), commentId);
+        log.info("이슈 댓글 삭제 요청 api 종료");
         return new ResponseEntity(CommandSuccessResponse.from("댓글이 삭제되었습니다."), OK);
     }
 
@@ -41,7 +47,9 @@ public class CommentController {
     public ResponseEntity modifyComment(@AuthenticationPrincipal UserPrincipal userPrincipal,
                                       @PathVariable Long commentId,
                                       @Validated @RequestBody ModifyCommentRequest request) {
+        log.info("이슈 댓글 수정 요청 api 시작");
         commentService.modifyComment(request.toServiceDto(userPrincipal.getId(), commentId));
+        log.info("이슈 댓글 수정 요청 api 종료");
         return new ResponseEntity(CommandSuccessResponse.from("댓글이 수정되었습니다."), OK);
     }
 }
--- api/src/main/java/com/kakaobean/issue/IssueController.java
+++ api/src/main/java/com/kakaobean/issue/IssueController.java
@@ -8,6 +8,7 @@ import com.kakaobean.core.issue.domain.repository.query.IssueQueryRepository;
 import com.kakaobean.issue.dto.ModifyIssueRequest;
 import com.kakaobean.issue.dto.RegisterIssueRequest;
 import com.kakaobean.security.UserPrincipal;
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
+@Timed("api.issue")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -28,27 +30,35 @@ public class IssueController {
 
     @PostMapping("/issues")
     public ResponseEntity registerIssue(@Validated @RequestBody RegisterIssueRequest request,
-                                        @AuthenticationPrincipal UserPrincipal userPrincipal){
+                                        @AuthenticationPrincipal UserPrincipal userPrincipal) {
+        log.info("이슈 생성 요청 api 시작");
         issueService.registerIssue(request.toServiceDto(userPrincipal.getId()));
+        log.info("이슈 생성 요청 api 종료");
         return new ResponseEntity(CommandSuccessResponse.from("이슈가 생성되었습니다."), CREATED);
     }
 
     @GetMapping("/issues/page")
-    public ResponseEntity findAllIssues(@RequestParam Long projectId, @RequestParam Integer page){
+    public ResponseEntity findAllIssues(@RequestParam Long projectId, @RequestParam Integer page) {
+        log.info("이슈 페이징 조회 api 시작");
         FindIssuesWithinPageResponseDto responseDto = issueQueryRepository.findByProjectId(projectId, page);
+        log.info("이슈 페이징 조회 api 종료");
         return new ResponseEntity(responseDto, OK);
     }
 
     @GetMapping("/issues/{issueId}")
-    public ResponseEntity findIssue(@PathVariable Long issueId){
+    public ResponseEntity findIssue(@PathVariable Long issueId) {
+        log.info("단일 이슈 조회 api 시작");
         FindIndividualIssueResponseDto responseDto = issueQueryRepository.findByIssueId(issueId);
+        log.info("단일 이슈 조회 api 종료");
         return new ResponseEntity(responseDto, OK);
     }
 
     @DeleteMapping("/issues/{issueId}")
     public ResponseEntity removeIssue(@AuthenticationPrincipal UserPrincipal userPrincipal,
                                        @PathVariable Long issueId) {
+        log.info("이슈 제거 api 시작");
         issueService.removeIssue(userPrincipal.getId(), issueId);
+        log.info("이슈 제거 api 종료");
         return new ResponseEntity(CommandSuccessResponse.from("이슈가 삭제되었습니다."), OK);
     }
 
@@ -56,7 +66,9 @@ public class IssueController {
     public ResponseEntity modifyIssue(@AuthenticationPrincipal UserPrincipal userPrincipal,
                                        @PathVariable Long issueId,
                                        @Validated @RequestBody ModifyIssueRequest request) {
+        log.info("이슈 수정 api 시작");
         issueService.modifyIssue(request.toServiceDto(userPrincipal.getId(), issueId));
+        log.info("이슈 수정 api 종료");
         return new ResponseEntity(CommandSuccessResponse.from("이슈 정보가 수정되었습니다."), OK);
     }
 
