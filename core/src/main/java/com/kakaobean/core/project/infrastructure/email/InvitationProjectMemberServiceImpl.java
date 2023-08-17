766aa6c7ce2367f13787e15cd9463b745bc15dc6
happysubin <asb0711@gmail.com>
2023-08-17 17:40:04 +0900

    fix: 초대 경로 수정

--- core/src/main/java/com/kakaobean/core/project/infrastructure/email/InvitationProjectMemberServiceImpl.java
+++ core/src/main/java/com/kakaobean/core/project/infrastructure/email/InvitationProjectMemberServiceImpl.java
@@ -1,27 +1,28 @@
 package com.kakaobean.core.project.infrastructure.email;
 
 import com.kakaobean.common.EmailHtmlUtils;
+import com.kakaobean.core.notification.config.EmailProperties;
 import com.kakaobean.core.project.domain.Project;
 import com.kakaobean.core.project.domain.service.InvitationProjectMemberService;
 import com.kakaobean.independentlysystem.email.EmailSender;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 
+@RequiredArgsConstructor
 @Component
 public class InvitationProjectMemberServiceImpl implements InvitationProjectMemberService {
 
     private final EmailSender emailSender;
+    private final EmailProperties emailProperties;
 
-    public InvitationProjectMemberServiceImpl(EmailSender emailSender) {
-        this.emailSender = emailSender;
-    }
 
     @Override
     public void sendInvitationMails(List<String> invitedMemberEmail, Project project) {
         String subject = "[코코노트] "+ project.getTitle()  + " 프로젝트 초대 메일입니다.";
-        String invitationUrl = "localhost:3000/invitations/" + project.getSecretKey(); //TODO 개발할 때 편의상 사용.
+        String invitationUrl = emailProperties.getHostName() + "/invitations/" + project.getSecretKey();
         emailSender.sendEmail(
                 invitedMemberEmail,
                 subject,
