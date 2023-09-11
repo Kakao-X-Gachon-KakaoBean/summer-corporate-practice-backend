23cba0bf3fa54587701ff85a45332c4adf2580e4
lib26 <dladlsqja@naver.com>
2023-09-11 22:00:47 +0900

    chore: 빌드 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,6 @@ JAR_NAME=$(basename $BUILD_JAR)
 echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/code-deploy/deploy.log
 
 echo "> build 파일 복사" >> /home/ec2-user/code-deploy/deploy.log
-DEPLOY_PATH=/home/ec2-user/code-deploy/
-cp $BUILD_JAR $DEPLOY_PATH
 
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/code-deploy/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
